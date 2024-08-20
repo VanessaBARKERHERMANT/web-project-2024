3280b17584fc3182601bbedfa42e85c8c7f6bc25
Vanessa <psr13306@students.ephec.be>
2024-08-20 22:09:02 +0200

    initialising backend

---
+++ CourseManagement/backend/Controllers/CourseController.cs
@@ -0,0 +1,47 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CourseController : ControllerBase
+{
+    private static List<Course> Courses = new List<Course>
+    {
+        new Course("Web", "Web is fun", "Developer"),
+        new Course("Security", "Security is important", "Admin"),
+        new Course("Network", "Network connects the world", "Admin"),
+        new Course("Database", "Database contains our data", "DBA"),
+    };
+
+    private readonly ILogger<CourseController> _logger;
+
+    public CourseController(ILogger<CourseController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Course> Get()
+    {
+        return Courses;
+    }
+
+    [HttpGet("ByName")]
+    public Course Get(string name)
+    {
+        return Courses.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+    }
+
+    [HttpPost]
+    public void Post(Course course)
+    {
+        //TODO backend validation
+        Courses.Add(course);
+    }
+
+
+
+
+}
---
+++ CourseManagement/backend/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿namespace backend.Models
+{
+    public class Course
+    {
+        public Course(string name, string description, string type)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
